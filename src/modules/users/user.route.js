1a4d523e308f79f1a147766045265790e7731468
david lepe <d.alconerolepe@gmail.com>
2025-08-19 14:49:13 -0600

    controller y  sesrvices de user

--- src/modules/users/user.route.js
+++ src/modules/users/user.route.js
@@ -1,7 +1,16 @@
-﻿import { Router } from "express";
+﻿// src/modules/users/user.route.js
+import { Router } from "express";
+import * as controller from "./user.controller.js";
+
 const router = Router();
 
-//Ruta de prueba
-router.get("/", (_req,res)=>res.json({ message: "List users (stub)" }));
+router.get("/", controller.getUsers);
+router.get("/:id", controller.getUserById);
+router.post("/", controller.createUser);
+router.put("/:id", controller.updateUser);
+router.delete("/:id", controller.deleteUser);
 
-export default router;
+// auth
+router.post("/auth/login", controller.login);
+
+export default router; // ← importante para que el import por default funcione
