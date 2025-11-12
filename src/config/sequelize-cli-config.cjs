9e5d1486e0be3bc495cf224e1eee24dad670698a
Keinaro <georgepor13@gmail.com>
2025-11-12 00:19:51 -0600

    refactor: consolidate database configuration for development and production environments

--- src/config/sequelize-cli-config.cjs
+++ src/config/sequelize-cli-config.cjs
@@ -1,14 +1,18 @@
 ﻿require("dotenv").config();
+
+const dbConfig = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3306,
+  dialect: "mysql",
+  dialectOptions: { decimalNumbers: true },
+  define: { charset: "utf8mb4", collate: "utf8mb4_0900_ai_ci" },
+  logging: console.log
+};
+
 module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 3306,
-    dialect: "mysql",
-    dialectOptions: { decimalNumbers: true },
-    define: { charset: "utf8mb4", collate: "utf8mb4_0900_ai_ci" },
-    logging: console.log
-  }
+  development: dbConfig,
+  production: dbConfig
 };
